eae16fb73f5544285bedad6e2e8aeb191aae16c9
kamrandarugar <kamrandarugar@gmail.com>
2024-02-01 15:23:54 -0800

    Setup Django environment

--- loosemeals/settings.py
+++ loosemeals/settings.py
@@ -11,20 +11,30 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import environ  # <-- Updated!
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Take environment variables from .env file
+environ.Env.read_env(BASE_DIR / '.env')  # <-- Updated!
+
+
+
+env = environ.Env(  # <-- Updated!
+    # set casting, default value
+    DEBUG=(bool, False),
+)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5s(epv--__2u0!dqf0zpzns#2=7v6zgg6erw@of0hbv=oh)k=3'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')  # <-- Updated!
 
 ALLOWED_HOSTS = []
 
@@ -78,8 +88,7 @@ WSGI_APPLICATION = 'loosemeals.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': env.db()
     }
 }
 
